46611ebf6f217630d9743509fad50b5cb3bc5518
cashcoder88 <cashglobe@gmail.com>
2019-06-14 09:19:21 -0600

    POST Register working, registered three users with hashed passwords

--- config/routes.js
+++ config/routes.js
@@ -1,6 +1,7 @@
 const axios = require('axios');
 const bcrypt = require('bcryptjs');
 const { authenticate } = require('../auth/authenticate');
+const Model = require('./model.js');
 
 module.exports = server => {
   server.post('/api/register', register);
@@ -10,7 +11,19 @@ module.exports = server => {
 
 function register(req, res) {
   // implement user registration
-  
+  let user = req.body;
+  const hash = bcrypt.hashSync(user.password, 8)
+  user.password = hash;
+
+  Model.add(user)
+  .then(user => {
+    res.status(201).json(user)
+  })
+  .catch(error => {
+    res.status(500).json({
+      error: 'A problem has occured with your registration, please try again'
+    })
+  })
 }
 
 function login(req, res) {
